9f93eaf1d32b2a99e4f24ddb6ccc2a6fafec2ee4
John Boyle <jtboyle.11@gmail.com>
2019-11-12 18:32:30 -0700

    Added Base Classes - Player, Weapon, Enemy, and Sprite with extra notes for further dev

--- testcode/application/Player.java
+++ testcode/application/Player.java
@@ -3,36 +3,16 @@ package application;
 import java.io.InputStream;
 
 import javafx.scene.image.Image;
-
-public class Player extends Image
+/*
+ *	AUTHORS: John Boyle, Colton Gerth, Levi Terry, Brendan Hagan
+ *	CLASS: Player.java
+ *	Purpose: Creates our user with basic characteristics and variables such as atk, def, hp and etc.
+ *	Extra Notes: Extends the Sprite class to make our player a sprite image
+ */
+public class Player extends Sprite
 {
-	//PLAYER IS A SPRITE WITH CERTAIN ATTRIBUTES SUCH AS HP
-	
-	public Player(InputStream is, double requestedWidth, double requestedHeight, boolean preserveRatio,
-			boolean smooth) {
-		super(is, requestedWidth, requestedHeight, preserveRatio, smooth);
-		// TODO Auto-generated constructor stub
+	public Player()
+	{
+		super("Images/Untitled_Artwork.png");
 	}
-	public Player(InputStream is) {
-		super(is);
-		// TODO Auto-generated constructor stub
-	}
-	public Player(String url, boolean backgroundLoading) {
-		super(url, backgroundLoading);
-		// TODO Auto-generated constructor stub
-	}
-	public Player(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth,
-			boolean backgroundLoading) {
-		super(url, requestedWidth, requestedHeight, preserveRatio, smooth, backgroundLoading);
-		// TODO Auto-generated constructor stub
-	}
-	public Player(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth) {
-		super(url, requestedWidth, requestedHeight, preserveRatio, smooth);
-		// TODO Auto-generated constructor stub
-	}
-	public Player(String url) {
-		super(url);
-		// TODO Auto-generated constructor stub
-	}
-
 }
