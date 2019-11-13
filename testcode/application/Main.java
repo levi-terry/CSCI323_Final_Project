bc99171088e533922c8a7cc55808c8bfc1e380cb
J526Tx <jtboyle.11@gmail.com>
2019-11-12 20:30:50 -0700

    Driver Class

--- testcode/application/Main.java
+++ testcode/application/Main.java
@@ -3,6 +3,8 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
 
 
@@ -16,6 +18,15 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			Canvas canvas = new Canvas( 512, 512 );
+		    root.getChildren().add( canvas );
+		 
+		    GraphicsContext gc = canvas.getGraphicsContext2D();
+			
+			//Instantiate player
+		    Player player = new Player();
+		    //render the player
+		    player.render(gc);
 			primaryStage.show();
 		} 
 		catch(Exception e) 
