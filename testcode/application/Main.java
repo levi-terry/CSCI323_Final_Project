cd38ef5c9bdfd62fc51541163cc8735ca056c257
John Boyle <jtboyle.11@gmail.com>
2019-11-14 10:20:23 -0700

    Added KeyListener and background to main

--- testcode/application/Main.java
+++ testcode/application/Main.java
@@ -45,12 +45,105 @@ public class Main extends Application {
 		    //render the player
 		    gc.drawImage(map, 0, 0);
 		    player.render(gc);
-
 			//Instantiate player
 			Player player1 = new Player();
 			//render the player
 			player1.render(gc);
+			
+			
+			//KEY LISTENER
+			/*
+			scene.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+
+				@Override
+				public void handle(KeyEvent event) {
+					// TODO Auto-generated method stub
+					String code = event.getCode().toString();
+					System.out.println(code + " Pressed");
+                    if(!input.contains(code))
+                    {
+                    	input.add(code);
+                    }
+				}
+			});
+			
+		    scene.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
+
+				@Override
+				public void handle(KeyEvent event) {
+					// TODO Auto-generated method stub
+					String code = event.getCode().toString();
+					System.out.println(code + " Released");
+					input.remove(code);
+				}
+			});
+		    
+		    
+		    
+		    new AnimationTimer() {
+				
+				@Override
+				public void handle(long now) {
+					// TODO Auto-generated method stub
+					if(input.contains("DOWN"))
+					{
+						player.setVelocity(0, 5);
+						player.move();
+						gc.clearRect(player.getX(), player.getY(), player.getWidth(), player.getHeight());
+						drawBackground(gc);
+						player.render(gc);
+					}
+					else
+					{
+						player.setVelocity(0, 0);
+					}
+					
+					if(input.contains("UP"))
+					{
+						player.setVelocity(0, -5);
+						player.move();
+						gc.clearRect(player.getX(), player.getY(), player.getWidth(), player.getHeight());
+						drawBackground(gc);
+						player.render(gc);
+					}
+					else
+					{
+						player.setVelocity(0, 0);
+					}
+					if(input.contains("LEFT"))
+					{
+						player.setVelocity(-5, 0);
+						player.move();
+						gc.clearRect(player.getX(), player.getY(), player.getWidth(), player.getHeight());
+						drawBackground(gc);
+						player.render(gc);
+					}
+					else
+					{
+						player.setVelocity(0, 0);
+					}
+					if(input.contains("RIGHT"))
+					{
+						player.setVelocity(5, 0);
+						player.move();
+						gc.clearRect(player.getX(), player.getY(), player.getWidth(), player.getHeight());
+						drawBackground(gc);
+						player.render(gc);
+					}
+					else
+					{
+						player.setVelocity(0, 0);
+					}
+					gc.setFill(null);
+					gc.fillText("Player Coords: (" + player.getX() + " : " + player.getY() + ")" , 100, 50);
+				}
+			}.start();
+			Canvas canvas = new Canvas( 512, 512 );
+			root.getChildren().add( canvas );
+
+			GraphicsContext gc = canvas.getGraphicsContext2D();*/
 			primaryStage.show();
+			
 		} 
 		catch(Exception e) 
 		{
